19627ab77db5eeb6ab205bcc323a01113930923a
fjamik <fjamik@gmail.com>
2019-05-29 09:56:15 -0400

    Updates

---
+++ precache-manifest.a742ecd35181e4392f29deb8175fada0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30835e80ff74592cbc5605a769df8b0e",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "2048637d00f0e6fac220",
+    "url": "/Clicky-Game/static/css/main.851a0d1f.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Clicky-Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "2048637d00f0e6fac220",
+    "url": "/Clicky-Game/static/js/main.bedb6e50.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  }
+]);
